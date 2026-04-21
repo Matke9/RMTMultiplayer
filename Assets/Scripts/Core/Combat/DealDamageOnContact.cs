040116b1c75e210709e38bfe7c763ae528fe0413
Mikric <nikolabus17@gmail.com>
2026-04-21 18:24:40 +0200

    2.13
    -Odradjeno ipaljivanje projetila
    -HealthComponent napravljen kao i HealthBar
    -Kreiran CoinBase prefab
    -Obrati paznju na layere

---
+++ Assets/Scripts/Core/Combat/DealDamageOnContact.cs
@@ -0,0 +1,30 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class DealDamageOnContact : MonoBehaviour
+{
+    [SerializeField] int damage = 1;
+
+
+    private ulong ownerClientId;
+
+    public void setOwner(ulong ownerClientId)
+    {
+        this.ownerClientId = ownerClientId;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.attachedRigidbody == null){return;}
+
+        if(collision.attachedRigidbody.TryGetComponent<NetworkObject>(out NetworkObject netObj))
+        {
+            if(ownerClientId == netObj.OwnerClientId) {return;}
+        }
+
+        if(collision.attachedRigidbody.TryGetComponent<Health>(out Health health))
+        {
+            health.TakeDamage(damage);
+        }
+    }
+}
--- Assets/Scripts/Core/Player/ProjectileLauncher.cs
+++ Assets/Scripts/Core/Player/ProjectileLauncher.cs
@@ -9,11 +9,17 @@ public class ProjectileLauncher : NetworkBehaviour
     [SerializeField] GameObject serverProjectilePrefab;
     [SerializeField] GameObject clientProjectilePrefab;
     [SerializeField] Transform firePoint;
+    [SerializeField] GameObject muzzleFlash;
+    [SerializeField] Collider2D playerCollider;
     
     [Header("Settings")]
     [SerializeField] float projectileSpeed;
+    [SerializeField] float fireRate;
+    [SerializeField] float muzzleFlashDuration;
     
     bool shouldFire = false;
+    float previousFireTime;
+    float muzzleFlashTimer;
 
     public override void OnNetworkSpawn()
     {
@@ -23,9 +29,23 @@ public class ProjectileLauncher : NetworkBehaviour
 
     void Update()
     {
+        if(muzzleFlashTimer > 0f)
+        {
+            muzzleFlashTimer -= Time.deltaTime;
+            if(muzzleFlashTimer <= 0f)
+            {
+                muzzleFlash.SetActive(false);
+            }
+        }
+
         if (!IsOwner) { return; }
+
         if (fireAction.WasPressedThisFrame())
         {
+            if(Time.time < (1/fireRate) + previousFireTime) { return; }
+
+            previousFireTime = Time.time;
+
             PrimaryFireServerRpc();
             SpawnDummyProjectile();
         }
@@ -36,6 +56,18 @@ public class ProjectileLauncher : NetworkBehaviour
     {
         GameObject projectileInstance = Instantiate(serverProjectilePrefab, 
             firePoint.position, firePoint.parent.parent.rotation, null);
+
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if(projectileInstance.TryGetComponent<DealDamageOnContact>(out DealDamageOnContact dealDamage))
+        {
+            dealDamage.setOwner(OwnerClientId);
+        }
+
+        if(projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up * projectileSpeed; 
+        }
         
         SpawnDummyProjectileClientRpc();
     }
@@ -47,9 +79,19 @@ public class ProjectileLauncher : NetworkBehaviour
         SpawnDummyProjectile();
     }
     
-    private void SpawnDummyProjectile()
+    void SpawnDummyProjectile()
     {
+        muzzleFlash.SetActive(true);
+        muzzleFlashTimer = muzzleFlashDuration;
+
         GameObject projectileInstance = Instantiate(clientProjectilePrefab, 
             firePoint.position, firePoint.parent.parent.rotation, null);
+
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+    
+        if(projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up * projectileSpeed; 
+        }
     }
 }
